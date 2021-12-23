15a6f38b756e56881d2ba39e8427ba56b27a5ee7
Vikt0r <vbachmanov@mail.ru>
2021-12-23 22:03:02 +0300

    Description page done

--- src/features/todos/description.js
+++ src/features/todos/description.js
@@ -4,6 +4,7 @@ import EditForm from './editForm';
 //import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 import { getTodoById } from './utils'
+import PriorityPic from './priorityPic'
 
 
 export default function Description() {
@@ -12,8 +13,16 @@ export default function Description() {
 
     let readView = (
         <Fragment>
-            <h2>{todoId}</h2>
-            <button className="button button_orange" onClick={handleEdit}>Edit</button>
+            <h2>{todoId} {todo.title}</h2>
+            <div className="description-bar">
+                <PriorityPic level={todo.priority} />
+                <div className="story-points-pic story-points-pic_theme-1">{todo.storyPoints}</div>
+                <div className="description-bar__status">{todo.status}</div>
+                
+                <button className="button button_orange description-bar__button" 
+                        onClick={handleEdit}>Edit</button>
+            </div>
+            <div className="description-text">{todo.description}</div>
         </Fragment>
     );
 
